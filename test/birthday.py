23985bd799d78e86f38de6b83e2234e8840542c7
krishnesh kumar <31180712+krishnesh-kumar@users.noreply.github.com>
2020-04-05 20:18:52 +0530

    Updated birthday_test
    Many conditions are removed in place of that a dictionary is added from that data is fetched.

--- test/birthday.py
+++ test/birthday.py
@@ -26,61 +26,85 @@ class todaydate:
         self.ago_month =  int(self.tdate.strftime("%m"))-int(self.bday_c.strftime("%m"))
         #self.from_now_month =  abs(self.ago_month)
         self.bdate=date
+        self.bday_dict={
+                        
+                        -1:"yesterday",
+                        
+                        -2:"day before yesterday",
+                        
+                        -3:f"{abs(self.days)} days back",-4:f"{abs(self.days)} days back",
+                        -5:f"{abs(self.days)} days back",-6:f"{abs(self.days)} days back",
+                        
+                        -7:f"last {self.bday_c.strftime('%A')}",
+                        
+                        -8:"In Last week",-9:"In Last week",-10:"In Last week",-11:"In Last week",
+                        -12:"In Last week",-13:"In Last week",
+                        
+                        -14:"2 weeks ago",-15:"2 weeks ago",-16:"2 weeks ago",-17:"2 weeks ago",
+                        -18:"2 weeks ago",-19:"2 weeks ago",-20:"2 weeks ago",
+                        
+                        -21:"3 weeks ago",-22:"3 weeks ago",-23:"3 weeks ago",-24:"3 weeks ago",
+                        -25:"3 weeks ago",-26:"3 weeks ago",-27:"3 weeks ago",
+                        
+                        -28:"Almost a month ago",-29:"Almost a month ago",-30:"Almost a month ago",
+                        
+                        -31:"last month",
+                        
+                        0:"Today",
+                        
+                        1:"Tomorrow",
+                        
+                        2:"day after tommorow",
+                        
+                        3:f"coming {self.bday_c.strftime('%A')}",4:f"coming {self.bday_c.strftime('%A')}",
+                        5:f"coming {self.bday_c.strftime('%A')}",6:f"coming {self.bday_c.strftime('%A')}",
+                        
+                        7:f"Next {self.bday_c.strftime('%A')}",8:f"Next {self.bday_c.strftime('%A')}",
+                        9:f"Next {self.bday_c.strftime('%A')}",10:f"Next {self.bday_c.strftime('%A')}",
+                        11:f"Next {self.bday_c.strftime('%A')}",12:f"Next {self.bday_c.strftime('%A')}",
+                        13:f"Next {self.bday_c.strftime('%A')}",
+                        
+                        14:"2 weeks from now",
+                        
+                        15:f"{self.days} days from now",16:f"{self.days} days from now",
+                        17:f"{self.days} days from now",18:f"{self.days} days from now",
+                        19:f"{self.days} days from now",20:f"{self.days} days from now",
+                        28:f"{self.days} days from now",
+                        30:f"{self.days} days from now",
+                        
+                        21:"3 Weeks from now",22:"After 3 weeks",23:"After 3 weeks",
+                        24:"After 3 weeks",25:"After 3 weeks",26:"After 3 weeks",27:"After 3 weeks",
+                        
+                        29:"Next month",31:"Next month",32:"Next month",33:"Next month",
+                        34:"Next month",35:"Next month",36:"Next month",37:"Next month",
+                        38:"Next month",39:"Next month",40:"Next month",41:"Next month",
+                        42:"Next month",43:"Next month",
 
+                        44:f"{2} month from now",45:f"{2} month from now",46:f"{2} month from now",
+                        47:f"{2} month from now",48:f"{2} month from now",49:f"{2} month from now",
+                        50:f"{2} month from now",51:f"{2} month from now",52:f"{2} month from now",
+                        53:f"{2} month from now",54:f"{2} month from now",55:f"{2} month from now",
+                        56:f"{2} month from now",57:f"{2} month from now",58:f"{2} month from now",
+                        59:f"{2} month from now",60:f"{2} month from now",61:f"{2} month from now",
+                        62:f"{2} month from now",63:f"{2} month from now",64:f"{2} month from now",
+                        65:f"{2} month from now",66:f"{2} month from now",67:f"{2} month from now",
+                        68:f"{2} month from now",69:f"{2} month from now",70:f"{2} month from now",
+                        71:f"{2} month from now",72:f"{2} month from now",73:f"{2} month from now",
+                        74:f"{2} month from now",
+                        
+                        }
     def reminder(self):
         '''
         This function contain the main logic of reminder app.
         '''
+    
     #************************B'DAY IS OVER********************************************
         
-        if self.days==-1:
+        if -30<=self.days<=-1:
             print(f"{self.tdate.strftime('%d')}-{self.tdate.strftime('%b')}-{self.tdate.strftime('%Y')}")
-            #print("yesterday")
             print(f"on {self.bday_c.strftime('%A')} {self.bday_c.strftime('%d')}th of {self.bday_c.strftime('%B')}")
-            return "yesterday"
-
-        elif self.days==-2:
-            print(f"{self.tdate.strftime('%d')}-{self.tdate.strftime('%b')}-{self.tdate.strftime('%Y')}")
-            #print("day before yesterday")
-            print(f"on {self.bday_c.strftime('%A')} {self.bday_c.strftime('%d')}th of {self.bday_c.strftime('%B')}")
-            return "day before yesterday"
-                
-        elif -6<=self.days<=-3:
-            print(f"{self.tdate.strftime('%d')}-{self.tdate.strftime('%b')}-{self.tdate.strftime('%Y')}")
-            #print(f"{abs(self.days)} days back")
-            print(f"on {self.bday_c.strftime('%A')} {self.bday_c.strftime('%d')}th of {self.bday_c.strftime('%B')}")
-            return f"{abs(self.days)} days back"
-
-        elif self.days==-7:
-            print(f"{self.tdate.strftime('%d')}-{self.tdate.strftime('%b')}-{self.tdate.strftime('%Y')}")
-            #print(f"last {self.bday_c.strftime('%A')}")
-            print(f"on {self.bday_c.strftime('%A')} {self.bday_c.strftime('%d')}th of {self.bday_c.strftime('%B')}")
-            return f"last {self.bday_c.strftime('%A')}"
-
-        elif -13<=self.days<=-8:
-            print(f"{self.tdate.strftime('%d')}-{self.tdate.strftime('%b')}-{self.tdate.strftime('%Y')}")
-            #print("Last week")
-            print(f"on {self.bday_c.strftime('%A')} {self.bday_c.strftime('%d')}th of {self.bday_c.strftime('%B')}")
-            return "In Last week"
-
-        elif -20<=self.days<=-14:
-            print(f"{self.tdate.strftime('%d')}-{self.tdate.strftime('%b')}-{self.tdate.strftime('%Y')}")
-            #print("2 weeks ago")
-            print(f"on {self.bday_c.strftime('%A')} {self.bday_c.strftime('%d')}th of {self.bday_c.strftime('%B')}")
-            return "2 weeks ago"
-            
-        elif -27<=self.days<=-21:
-            print(f"{self.tdate.strftime('%d')}-{self.tdate.strftime('%b')}-{self.tdate.strftime('%Y')}")
-            #print("3 weeks ago")
-            print(f"on {self.bday_c.strftime('%A')} {self.bday_c.strftime('%d')}th of {self.bday_c.strftime('%B')}")
-            return "3 weeks ago"    
-            
-        elif -31<self.days<=-28:
-            print(f"{self.tdate.strftime('%d')}-{self.tdate.strftime('%b')}-{self.tdate.strftime('%Y')}")
-            #print("Almost a month ago")
-            print(f"on {self.bday_c.strftime('%A')} {self.bday_c.strftime('%d')}th of {self.bday_c.strftime('%B')}")
-            return "Almost a month ago"
-
+            return self.bday_dict[self.days]
+        
         elif self.days<=-31 and self.ago_month==1:
             print(f"{self.tdate.strftime('%d')}-{self.tdate.strftime('%b')}-{self.tdate.strftime('%Y')}")
             #print("last month")
@@ -89,87 +113,22 @@ class todaydate:
             
         elif self.days<-31:
             print(f"{self.tdate.strftime('%d')}-{self.tdate.strftime('%b')}-{self.tdate.strftime('%Y')}")
-            #print(f"{self.ago_month} month ago")
             print(f"on {self.bday_c.strftime('%A')} {self.bday_c.strftime('%d')}th of {self.bday_c.strftime('%B')}")
             return f"{self.ago_month} month ago"
             
-        #************************TODAY IS B'DAY**********************************************
-        elif self.days==0:
+    #************************B'DAY STILL TO COME*****************************************
+        
+        elif 0<=self.days<=74:
             print(f"{self.tdate.strftime('%d')}-{self.tdate.strftime('%b')}-{self.tdate.strftime('%Y')}")
-            #print("TODAY")
             print(f"on {self.bday_c.strftime('%A')} {self.bday_c.strftime('%d')}th of {self.bday_c.strftime('%B')}")
-            return "TODAY"
+            return self.bday_dict[self.days]
             
-        #************************B'DAY STILL TO COME*****************************************
-
-        elif self.days==1:
-            print(f"{self.tdate.strftime('%d')}-{self.tdate.strftime('%b')}-{self.tdate.strftime('%Y')}")
-            #print("Tomorrow")
-            print(f"on {self.bday_c.strftime('%A')} {self.bday_c.strftime('%d')}th of {self.bday_c.strftime('%B')}")
-            return "Tomorrow"
-            
-        elif self.days==2:
-            print(f"{self.tdate.strftime('%d')}-{self.tdate.strftime('%b')}-{self.tdate.strftime('%Y')}")
-            #print("day after tommorow")
-            print(f"on {self.bday_c.strftime('%A')} {self.bday_c.strftime('%d')}th of {self.bday_c.strftime('%B')}")
-            return "day after tommorow"
-
-        elif 3<=self.days<=6:
-            print(f"{self.tdate.strftime('%d')}-{self.tdate.strftime('%b')}-{self.tdate.strftime('%Y')}")
-            #print(f"coming {self.bday_c.strftime('%A')}")
-            print(f"on {self.bday_c.strftime('%A')} {self.bday_c.strftime('%d')}th of {self.bday_c.strftime('%B')}")
-            return f"coming {self.bday_c.strftime('%A')}"
-        
-        elif 7<=self.days<=13:
-            print(f"{self.tdate.strftime('%d')}-{self.tdate.strftime('%b')}-{self.tdate.strftime('%Y')}")
-            #print(f"Next {self.bday_c.strftime('%A')}")
-            print(f"on {self.bday_c.strftime('%A')} {self.bday_c.strftime('%d')}th of {self.bday_c.strftime('%B')}")
-            return f"Next {self.bday_c.strftime('%A')}"
-        
-        elif 15<=self.days<=20 or self.days==30 or self.days==28:
-            print(f"{self.tdate.strftime('%d')}-{self.tdate.strftime('%b')}-{self.tdate.strftime('%Y')}")
-            #print(f"{self.days} days from now")
-            print(f"on {self.bday_c.strftime('%A')} {self.bday_c.strftime('%d')}th of {self.bday_c.strftime('%B')}")
-            return f"{self.days} days from now"
-        
-        elif self.days==14:
-            print(f"{self.tdate.strftime('%d')}-{self.tdate.strftime('%b')}-{self.tdate.strftime('%Y')}")
-            #print("2 weeks from now")
-            print(f"on {self.bday_c.strftime('%A')} {self.bday_c.strftime('%d')}th of {self.bday_c.strftime('%B')}")
-            return "2 weeks from now"
-        
-        elif self.days==21:
-            print(f"{self.tdate.strftime('%d')}-{self.tdate.strftime('%b')}-{self.tdate.strftime('%Y')}")
-            #print("3 Weeks from now")
-            print(f"on {self.bday_c.strftime('%A')} {self.bday_c.strftime('%d')}th of {self.bday_c.strftime('%B')}")
-            return "3 Weeks from now"
-        
-        elif 22<=self.days<=27:
-            print(f"{self.tdate.strftime('%d')}-{self.tdate.strftime('%b')}-{self.tdate.strftime('%Y')}")
-            #print("After 3 weeks")
-            print(f"on {self.bday_c.strftime('%A')} {self.bday_c.strftime('%d')}th of {self.bday_c.strftime('%B')}")
-            return "After 3 weeks"
-
-        elif 31<=self.days<=43 or self.days==29:
-            print(f"{self.tdate.strftime('%d')}-{self.tdate.strftime('%b')}-{self.tdate.strftime('%Y')}")
-            #print("Next month")
-            print(f"on {self.bday_c.strftime('%A')} {self.bday_c.strftime('%d')}th of {self.bday_c.strftime('%B')}")
-            return "Next month"
-        
-        #and self.days/30>1
-        elif 44<=self.days<=74:
-            print(f"{self.tdate.strftime('%d')}-{self.tdate.strftime('%b')}-{self.tdate.strftime('%Y')}")
-            #print(f"{math.ceil(self.days/30)} month from now")
-            print(f"on {self.bday_c.strftime('%A')} {self.bday_c.strftime('%d')}th of {self.bday_c.strftime('%B')}")
-            return f"{2} month from now"
-
         elif self.days>=75 and self.days/30>1:
             print(f"{self.tdate.strftime('%d')}-{self.tdate.strftime('%b')}-{self.tdate.strftime('%Y')}")
-            #print(f"{math.ceil(self.days/30)} month from now")
             print(f"on {self.bday_c.strftime('%A')} {self.bday_c.strftime('%d')}th of {self.bday_c.strftime('%B')}")
             return f"{math.ceil(self.days/30)} month from now"
         
 
 if __name__=="__main__":    
-    b=Bdate("1-1-1995")
+    b=todaydate("1-1-1995")
     print(b.reminder())
